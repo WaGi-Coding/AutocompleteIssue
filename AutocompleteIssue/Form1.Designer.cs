3b46e7f03e00b2e0bca55c6aa88b0df6164c9aa6
WaGi <wagi.coding@gmail.com>
2021-01-27 00:41:12 +0100

    Add project files.

---
+++ AutocompleteIssue/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace AutocompleteIssue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox1.Size = new System.Drawing.Size(479, 178);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "help",
+            "alive",
+            "listen allon",
+            "listen alloff",
+            "listen login",
+            "listen matchstate",
+            "listen killfeed",
+            "listen scorefeed",
+            "listen chat",
+            "listen custom",
+            "listen punishment",
+            "listenstatus",
+            "adminlist",
+            "banlist",
+            "mutelist",
+            "playerlist",
+            "maplist",
+            "addmap ",
+            "removemap ",
+            "addadmin ",
+            "removeadmin ",
+            "addbots ",
+            "removebots ",
+            "changelevel ",
+            "say ",
+            "customsay ",
+            "string ",
+            "event ",
+            "spawnserveractor ",
+            "kick ",
+            "ban ",
+            "unban ",
+            "mute ",
+            "unmute ",
+            "scoreboard",
+            "info",
+            "stats",
+            "writetoconsole ",
+            "changeteam ",
+            "modifyinivar ",
+            "getmatchduration",
+            "extendmatchduration ",
+            "setmotd ",
+            "teleportplayer ",
+            "killplayer ",
+            "getteamdamage ",
+            "renameplayer ",
+            "mountpak ",
+            "chatlog ",
+            "shutdown"});
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox1.Location = new System.Drawing.Point(13, 197);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(370, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 229);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AutocompleteIssue/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutocompleteIssue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += textBox1.Text + Environment.NewLine;
+            textBox1.Text = string.Empty;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                e.SuppressKeyPress = true;
+                textBox1.SelectAll();
+            }
+
+            // This is also the case if selecting a suggestion from autocomplete dropdown. I won't send instantly, but i want to keep pressing Enter to send in the end
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+
+                // This is not what i want
+                //if (textBox1.AutoCompleteCustomSource.Contains(textBox1.Text) && textBox1.Text.EndsWith(" "))
+                //{
+                //    textBox1.SelectionStart = textBox1.Text.Length;
+                //    return;
+                //}
+
+                button1_Click(sender, null);
+            }
+        }
+
+
+
+        protected override bool ProcessTabKey(bool forward)
+        {
+            Control ctl = this.ActiveControl;
+            if (ctl != null && ctl == textBox1 && !string.IsNullOrEmpty(textBox1.Text))
+            {
+                TextBox tb = (TextBox)ctl;
+                tb.Focus();
+                tb.SelectionStart = tb.Text.Length;
+                return true;
+            }
+            return base.ProcessTabKey(forward); // process TAB key as normal
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = textBox1;
+        }
+    }
+
+
+}
